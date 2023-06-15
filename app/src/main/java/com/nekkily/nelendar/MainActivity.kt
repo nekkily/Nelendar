7c16e5b1192c321d7c9a6f496c9f14b5a31677a4
nekkily <nekkily@gmail.com>
2023-06-15 22:08:23 +0300

    #calendar nekkily: create calendar library

--- app/src/main/java/com/nekkily/nelendar/MainActivity.kt
+++ app/src/main/java/com/nekkily/nelendar/MainActivity.kt
@@ -17,7 +17,6 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.nekkily.nelendar.ui.FirstDayOfWeek
-import com.nekkily.nelendar.ui.theme.NelendarTheme
 import com.nekkily.nelendar.ui.theme.*
 import com.nekkily.nelendar.ui.view.Calendar
 

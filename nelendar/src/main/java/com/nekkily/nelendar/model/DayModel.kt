f4e111db4c1beffdedc5f6866f261cfcc6d575b2
nekkily <nekkily@gmail.com>
2023-09-05 22:06:55 +0300

    #dev nekkily: add documentation for day model and small refactoring

--- nelendar/src/main/java/com/nekkily/nelendar/model/DayModel.kt
+++ nelendar/src/main/java/com/nekkily/nelendar/model/DayModel.kt
@@ -2,6 +2,12 @@ package com.nekkily.nelendar.model
 
 import java.util.Date
 
+/**
+ * Day model class.
+ *
+ * @param date [Date] contains full date of the day.
+ * @param isCurrentMonth [Boolean] indicates if this day belongs to month that prevails on the screen.
+ */
 data class DayModel(
     val date: Date,
     val isCurrentMonth: Boolean
--- nelendar/src/main/java/com/nekkily/nelendar/ui/theme/Theme.kt
+++ nelendar/src/main/java/com/nekkily/nelendar/ui/theme/Theme.kt
@@ -3,41 +3,11 @@ package com.nekkily.nelendar.ui.theme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 
-/*private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
-
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-*/
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-
-)*/
-
 @Composable
 fun NelendarTheme(
-    //darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    /*val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }*/
-
     MaterialTheme(
-        //colors = colors,
-        //typography = Typography,
         shapes = Shapes,
         content = content
     )
